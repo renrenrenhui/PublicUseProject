ec14c1d66c163890fa5ad434d4f1c4785654c3ee
renhui <1025975368@qq.com>
2017-05-16 11:08:23 +0800

    Add auto text size feature

--- app/src/main/java/com/example/xiaoniu/publicuseproject/MainActivity.java
+++ app/src/main/java/com/example/xiaoniu/publicuseproject/MainActivity.java
@@ -6,10 +6,13 @@ import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv2;
     private String msg ="在Android开发中，有许多信息展示需要通过TextView来展现，如果只是普通的信息展现，使用TextView setText(CharSequence str)设置即可，但是当在TextView里的这段内容需要截取某一部分字段，可以被点击以及响应响应的操作，这时候就需要用到SpannableString了，下面通过一段简单的代码实现部分文字被点击响应，及富文本表情的实现";
+    private TextView mOutput, mAutofitOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +81,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         /*end for changeTheme*/
+
+        /*start for auto textview*/
+        mOutput = (TextView)findViewById(R.id.output);
+        mAutofitOutput = (TextView)findViewById(R.id.output_autofit);
+        ((EditText)findViewById(R.id.input)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                // do nothing
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                mOutput.setText(charSequence);
+                mAutofitOutput.setText(charSequence);
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // do nothing
+            }
+        });
+        /*end for auto textview*/
     }
 
     /**
